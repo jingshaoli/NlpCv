7e6cb4df8ef6f73048c6d54b362508b28a2bdd8b
jingshaoli <beijinglijingshao@yaoo.co.jp>
2019-07-17 15:50:04 +0900

    finish assign01 and 02

--- lesson1.py
+++ lesson1.py
@@ -167,7 +167,7 @@ def pat_match(pattern = 'My ?X told me something',saying= 'My mother is fuking y
         else:
             return pat_match(pattern[1:],saying[1:])
 
-pat_match('I want ?X a a'.split(),"I want holiday s s".split())
+pat_match('?X want holiday'.split(),"who want holiday".split())
 
 def pat_to_dict(patterns):
     return {k:v for k,v in patterns}
@@ -186,7 +186,7 @@ defined_patterns = {
 
 def get_response(saying,defined_patterns):
     for rule in defined_patterns.keys():
-        if pat_match('My ?X told me something'.split(),saying.split()):
+        if pat_match(rule.split(),saying.split()):
             ans = choice(defined_patterns[rule])
             got_patterns = pat_match(rule.split(),saying.split())
             continue
@@ -194,9 +194,8 @@ def get_response(saying,defined_patterns):
         return ' '.join(subsitite(ans.split(), pat_to_dict(got_patterns)))
     except UnboundLocalError:
         return print('no matching rule')
-    else:
-        return ' '.join(subsitite(ans.split(), pat_to_dict(got_patterns)))
-get_response('My mother is fuking you',defined_patterns)
+
+get_response('My mother told me something',defined_patterns)
 
 #=============================================================================--
 def is_pattern_segment(pattern):
@@ -229,9 +228,9 @@ def segment_match(pattern, saying):
     
     for i, token in enumerate(saying):
         if rest[0] == token and is_match(rest[1:], saying[(i + 1):]):
-            return (seg_pat, saying[:i]), i
+            return (seg_pat, saying[:i]), i,0
     
-    return (seg_pat, saying), len(saying)
+    return (seg_pat, saying), len(saying),1
 
 def is_match(rest, saying):
     if not rest and not saying:
@@ -242,13 +241,169 @@ def is_match(rest, saying):
         return False
     return is_match(rest[1:], saying[1:])
 
-def strStr(source="aadf", target="a"):
-    longth = len(target)
-    for i in range(len(source)-longth+1):
-#        print(i)
-        if source[i:i+longth] == target:
-            print(i)
-            return i
-    return -1
+segment_match('?*P is very good'.split(), "My dog and my cat is very good".split())
 
-sorted([2,3,4,1,2])
+pat_match_with_seg('?*P is very good and ?*X'.split(), "My dog is very good and my cat is very cute".split())
+
+response_pair = {
+    'I need ?X': [
+        "Why do you neeed ?X"
+    ],
+    "I dont like my ?X": ["What bad things did ?X do for you?"]
+}
+
+pat_match_with_seg('I need ?*X'.split(), 
+                  "I need an iPhone".split())
+
+subsitite("Why do you neeed ?X".split(), pat_to_dict(pat_match_with_seg('I need ?*X'.split(), 
+                  "I need an iPhone".split())))
+
+def pat_to_dict(patterns):
+    return {k: ' '.join(v) if isinstance(v, list) else v for k, v in patterns}
+
+subsitite("Why do you neeed ?X".split(), pat_to_dict(pat_match_with_seg('I need ?*X'.split(), 
+                  "I need an iPhone".split())))
+
+subsitite("Hi, how do you do?".split(), pat_to_dict(pat_match_with_seg('?*X hello ?*Y'.split(), 
+                  "I am mike, hello ".split())))
+
+defined_patterns_chinese = {
+    '?*x我想?*y': ['你觉得?y有什么意义呢？', '为什么你想?y', '你可以想想你很快就可以?y了'],
+    '?*x我想要?*y': ['?x想问你，你觉得?y有什么意义呢?', '为什么你想?y', '?x觉得... 你可以想想你很快就可以有?y了', '你看?x像?y不', '我看你就像?y'],
+    '?*x喜欢?*y': ['喜欢?y的哪里？', '?y有什么好的呢？', '你想要?y吗？'],
+    '?*x讨厌?*y': ['?y怎么会那么讨厌呢?', '讨厌?y的哪里？', '?y有什么不好呢？', '你不想要?y吗？'],
+    '?*xAI?*y': ['你为什么要提AI的事情？', '你为什么觉得AI要解决你的问题？'],
+    '?*x机器人?*y': ['你为什么要提机器人的事情？', '你为什么觉得机器人要解决你的问题？'],
+    '?*x对不起?*y': ['不用道歉', '你为什么觉得你需要道歉呢?'],
+    '?*x我记得?*y': ['你经常会想起这个吗？', '除了?y你还会想起什么吗？', '你为什么和我提起?y'],
+    '?*x如果?*y': ['你真的觉得?y会发生吗？', '你希望?y吗?', '真的吗？如果?y的话', '关于?y你怎么想？'],
+    '?*x我?*z梦见?*y':['真的吗? --- ?y', '你在醒着的时候，以前想象过?y吗？', '你以前梦见过?y吗'],
+    '?*x妈妈?*y': ['你家里除了?y还有谁?', '嗯嗯，多说一点和你家里有关系的', '她对你影响很大吗？'],
+    '?*x爸爸?*y': ['你家里除了?y还有谁?', '嗯嗯，多说一点和你家里有关系的', '他对你影响很大吗？', '每当你想起你爸爸的时候， 你还会想起其他的吗?'],
+    '?*x我愿意?*y': ['我可以帮你?y吗？', '你可以解释一下，为什么想?y'],
+    '?*x我很难过，因为?*y': ['我听到你这么说， 也很难过', '?y不应该让你这么难过的'],
+    '?*x难过?*y': ['我听到你这么说， 也很难过',
+                 '不应该让你这么难过的，你觉得你拥有什么，就会不难过?',
+                 '你觉得事情变成什么样，你就不难过了?'],
+    '?*x就像?*y': ['你觉得?x和?y有什么相似性？', '?x和?y真的有关系吗？', '怎么说？'],
+    '?*x和?*y都?*z': ['你觉得?z有什么问题吗?', '?z会对你有什么影响呢?'],
+    '?*x和?*y一样?*z': ['你觉得?z有什么问题吗?', '?z会对你有什么影响呢?'],
+    '?*x我是?*y': ['真的吗？', '?x想告诉你，或许我早就知道你是?y', '你为什么现在才告诉我你是?y'],
+    '?*x我是?*y吗': ['如果你是?y会怎么样呢？', '你觉得你是?y吗', '如果你是?y，那一位着什么?'],
+    '?*x你是?*y吗':  ['你为什么会对我是不是?y感兴趣?', '那你希望我是?y吗', '你要是喜欢， 我就会是?y'],
+    '?*x你是?*y' : ['为什么你觉得我是?y'],
+    '?*x因为?*y' : ['?y是真正的原因吗？', '你觉得会有其他原因吗?'],
+    '?*x我不能?*y': ['你或许现在就能?*y', '如果你能?*y,会怎样呢？'],
+    '?*x我觉得?*y': ['你经常这样感觉吗？', '除了到这个，你还有什么其他的感觉吗？'],
+    '?*x我?*y你?*z': ['其实很有可能我们互相?y'],
+    '?*x你为什么不?*y': ['你自己为什么不?y', '你觉得我不会?y', '等我心情好了，我就?y'],
+    '?*x好的?*y': ['好的', '你是一个很正能量的人'],
+    '?*x嗯嗯?*y': ['好的', '你是一个很正能量的人'],
+    '?*x不嘛?*y': ['为什么不？', '你有一点负能量', '你说 不，是想表达不想的意思吗？'],
+    '?*x不要?*y': ['为什么不？', '你有一点负能量', '你说 不，是想表达不想的意思吗？'],
+    '?*x有些人?*y': ['具体是哪些人呢?'],
+    '?*x有的人?*y': ['具体是哪些人呢?'],
+    '?*x某些人?*y': ['具体是哪些人呢?'],
+    '?*x每个人?*y': ['我确定不是人人都是', '你能想到一点特殊情况吗？', '例如谁？', '你看到的其实只是一小部分人'],
+    '?*x所有人?*y': ['我确定不是人人都是', '你能想到一点特殊情况吗？', '例如谁？', '你看到的其实只是一小部分人'],
+    '?*x总是?*y': ['你能想到一些其他情况吗?', '例如什么时候?', '你具体是说哪一次？', '真的---总是吗？'],
+    '?*x一直?*y': ['你能想到一些其他情况吗?', '例如什么时候?', '你具体是说哪一次？', '真的---总是吗？'],
+    '?*x或许?*y': ['你看起来不太确定'],
+    '?*x可能?*y': ['你看起来不太确定'],
+    '?*x他们是?*y吗？': ['你觉得他们可能不是?y？'],
+    '?*x': ['很有趣', '请继续', '我不太确定我很理解你说的, 能稍微详细解释一下吗?'],
+    '?*x你好?*y': ['你好呀', '请告诉我你的问题']
+}
+
+def string2rule(rule):
+    string = []
+    rule = cut(rule)
+    for i,word in enumerate(rule):
+        if i == 0:
+            string.append(word)
+        else:
+            if '\u4e00' <= word <= '\u9fff' and (string[-1] > '\u9fff' or string[-1] < '\u4e00'):
+                string.append(' ')
+                string.append(word)
+            elif '\u4e00' <= string[-1] <= '\u9fff' and ('\u4e00' <= word <= '\u9fff'):
+                string.append(' ')
+                string.append(word)
+            elif (word > '\u9fff' or word < '\u4e00') and '\u4e00' <= string[-1] <= '\u9fff':
+                string.append(' ')
+                string.append(word)
+            else:string.append(word)
+    rule = ''.join(string)
+    return rule
+                
+def get_response_chinese(saying_chinese,defined_patterns_chinese):
+    saying_jieba = ' '.join(cut(saying_chinese))
+#    print(saying)
+    for rule in defined_patterns_chinese.keys():
+        rule_jieba = string2rule(rule)
+        if pat_match_with_seg(rule_jieba.split(),saying_jieba.split()):
+            print(rule)
+            ans = choice(defined_patterns_chinese[rule])
+            ans_jieba = ' '.join(cut(ans))
+            got_patterns = pat_match_with_seg(rule_jieba.split(),saying_jieba.split())
+            continue
+    try:
+        return ' '.join(subsitite(ans_jieba.split(), pat_to_dict(got_patterns)))
+    except UnboundLocalError:
+        return print('no matching rule')
+
+get_response_chinese('他说我是傻逼',defined_patterns_chinese)
+saying_chinese,defined_patterns_chinese = '他说我是傻逼',defined_patterns_chinese
+rule = '?*x你好?*y'
+
+
+defined_patterns = {
+    "I need ?X": ["Image you will get ?X soon", "Why do you need ?X ?"], 
+    "?*X told me something": ["Talk about more about ?X", "How do you think about ?X ?"]
+}
+
+def get_response(saying,defined_patterns):
+    for rule in defined_patterns.keys():
+        if pat_match_with_seg(rule.split(),saying.split()):
+            ans = choice(defined_patterns[rule])
+            got_patterns = pat_match_with_seg(rule.split(),saying.split())
+            continue
+    try:
+        return ' '.join(subsitite(ans.split(), pat_to_dict(got_patterns)))
+    except UnboundLocalError:
+        return print('no matching rule')
+
+get_response('His mother told you something',defined_patterns)
+saying,rule = 'His mother told me something',"?*X told me something"
+
+def is_pattern_segment(pattern):
+    return pattern.startswith('?*') and all(a.isalpha() for a in pattern[2:])
+pattern = pat
+def pat_match_with_seg(pattern, saying):
+    if not pattern or not saying: return []
+    
+    pat = pattern[0]
+    
+    if is_variable(pat):
+        return [(pat, saying[0])] + pat_match_with_seg(pattern[1:], saying[1:])
+    elif is_pattern_segment(pat):
+        match, index = segment_match(pattern, saying)
+        return [match] + pat_match_with_seg(pattern[1:], saying[index:])
+    elif pat == saying[0]:
+        return pat_match_with_seg(pattern[1:], saying[1:])
+    else:
+        return fail
+
+pattern, saying = rule.split(),saying.split()
+
+def segment_match(pattern, saying):
+    seg_pat, rest = pattern[0], pattern[1:]
+    seg_pat = seg_pat.replace('?*', '?')
+
+    if not rest: return (seg_pat, saying), len(saying)    
+    
+    for i, token in enumerate(saying):
+        if rest[0] == token and is_match(rest[1:], saying[(i + 1):]):
+            return (seg_pat, saying[:i]), i,0
+        
+    
+    return (seg_pat, saying), len(saying),1
